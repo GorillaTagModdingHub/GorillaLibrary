8c176185e3a0a432f7d9a36fbfe85376b1e4bf24
Lapis <47952866+LapisGit@users.noreply.github.com>
2026-03-04 07:53:25 -0500

    Add files via upload

---
+++ GorillaLibrary.Web/ERequestType.cs
@@ -0,0 +1,15 @@
+namespace GorillaLibrary.Web
+{
+    public enum ERequestType
+    {
+        Get,
+        Post,
+        Put,
+        Delete,
+        Patch,
+        Head,
+        Options,
+        Trace,
+        Connect
+    }
+}
