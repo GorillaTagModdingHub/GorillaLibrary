58167cdf579e23dbedc33dc97ee91548ce78ce0e
liquidsquid1 <151129309+liquidsquid1@users.noreply.github.com>
2026-03-04 20:22:56 +0000

    forgot to hit ctrl+s

--- GorillaLibrary/Events/RoomEvents.cs
+++ GorillaLibrary/Events/RoomEvents.cs
@@ -1,10 +1,22 @@
 ﻿using GorillaLibrary.Events.System;
+using System;
 
 namespace GorillaLibrary.Events
 {
     public class RoomEvents
     {
-        public class RoomJoinedEvent : IEvent { }
-        public class RoomLeftEvent : IEvent { }
+        public class RoomJoinedEvent : IEvent { 
+            public static void Initialize()
+            {
+                NetworkSystem.Instance.OnMultiplayerStarted += () => GorillaLibrary.Instance.Bus.Publish(new RoomJoinedEvent());
+            }
+        }
+        public class RoomLeftEvent : IEvent
+        {
+            public static void Initialize()
+            {
+                NetworkSystem.Instance.OnReturnedToSinglePlayer += () => GorillaLibrary.Instance.Bus.Publish(new RoomLeftEvent());
+            }
+        }
     }
 }
--- GorillaLibrary/Events/System/EventBus.cs
+++ GorillaLibrary/Events/System/EventBus.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace GorillaLibrary.Events
+namespace GorillaLibrary.Events.System
 {
     public class EventBus
     {
