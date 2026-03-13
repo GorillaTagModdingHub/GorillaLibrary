7a2442a076b0588d20363518015d0ae7dee77505
Dane "Dev <81720436+developer9998@users.noreply.github.com>
2026-03-13 01:16:35 -0230

    minor changes

---
+++ GorillaLibrary.Content/Mod.cs
@@ -0,0 +1,14 @@
+﻿using GorillaLibrary.Content;
+using MelonLoader;
+using System;
+
+[assembly: MelonInfo(typeof(Mod), "GorillaLibrary.Content", "1.0.0", "dev9998")]
+[assembly: MelonGame("Another Axiom", "Gorilla Tag")]
+[assembly: MelonAdditionalDependencies("GorillaLibrary")]
+
+namespace GorillaLibrary.Content;
+
+public class Mod : MelonMod
+{
+    
+}
